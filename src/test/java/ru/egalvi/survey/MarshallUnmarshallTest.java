0a1c8497971287ef20507d9004705414b331bf25
Alekzander <egalvi@mail.ru>
2017-02-19 20:51:51 +0300

    Marshalling-unmarshalling test

---
+++ src/test/java/ru/egalvi/survey/MarshallUnmarshallTest.java
@@ -0,0 +1,24 @@
+package ru.egalvi.survey;
+
+import org.junit.Test;
+import ru.egalvi.survey.model.Survey;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+public class MarshallUnmarshallTest {
+
+    @Test
+    public void testMarshallUnmarshall() throws Exception {
+        //Create JAXB context
+        JAXBContext jc = JAXBContext.newInstance(Survey.class);
+        //Unmarshall an XML file
+        Unmarshaller unmarshaller = jc.createUnmarshaller();
+        Survey survey = (Survey) unmarshaller.unmarshal(Thread.currentThread().getContextClassLoader().getResourceAsStream("questions.xml"));
+        //Marshall to XML
+        Marshaller marshaller = jc.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(survey, System.out);
+    }
+}
